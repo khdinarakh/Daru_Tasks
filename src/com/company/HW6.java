b38a8806c9c931813382b6e3e0c9cf5109d29d04
khdinarakh <nis.dinara2002@gmial.com>
2023-10-27 15:21:37 +0600

    HW6 changes

--- src/com/company/HW6.java
+++ src/com/company/HW6.java
@@ -95,7 +95,7 @@ public class HW6 {
         System.out.println("The first min value: " + firstMin);
     }
 
-    public void printSecondMinValue(int[] arr) {
+    public int printSecondMinValue(int[] arr) {
         int secondMin = arr[0];
         int firstMin = findMinArr(arr);
 
@@ -110,15 +110,16 @@ public class HW6 {
         }
         if (arr.length == 1) {
             System.out.println("There are only one min");
+            return secondMin;
         }
-        System.out.println("The second min: " + secondMin);
+        return secondMin;
     }
 
     //Дан массив целых значений. Напишите программу для формирования наибольшего числа из заданного массива целых неотрицательных чисел.
 
     //bubble sort
 
-    public void descOrder(int[] arr) {
+    public int[] descOrder(int[] arr) {
         int temp;
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
@@ -129,10 +130,22 @@ public class HW6 {
                 }
             }
         }
-        System.out.println("Sort desc: ");
-        for (int number : arr) {
-            System.out.print(number + " ");
+        return arr;
+    }
+
+    //for future
+    public int[] ascOrder(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
+        return arr;
     }
 
     // 0:0 0:1
@@ -140,30 +153,42 @@ public class HW6 {
     // 2:0 2:1
     //Дан двумерный массив целых значений. Напишите программу для печати массива после изменения строк и столбцов заданного двумерного массива.
 
-    public void changeRowToColumn(int[][] arr) {
+    /*public int[][] changeRowToColumn(int[][] arr) {
+        int row = arr.length;
+        int column = arr[0].length;
+        int[][] changeRowToColumn = new int[column][row];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                changeRowToColumn[j][i] = arr[i][j];
+            }
+        }
+        return changeRowToColumn;
+    }*/
+
+    public int[][] changeRowToColumn(int[][] arr) {
         System.out.println();
         int row = arr.length;
         int column = arr[0].length;
-        ;
-
+        int[][] changeRowToColumn = new int[column][row];
         for (int i = 0; i < column; i++) {
             for (int j = 0; j < row; j++) {
-                System.out.print(arr[j][i] + " ");
+                changeRowToColumn[i][j] = arr[j][i];
             }
-            System.out.println();
         }
+        return changeRowToColumn;
     }
 
     //Напишите программу, которая возвращает сумму всех значений в двумерном массиве.
 
-    public void sumTwoDArr(int[][] arr) {
+    public int sumTwoDArr(int[][] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 sum += arr[i][j];
             }
         }
-        System.out.println("Sum of two dimensional array: " + sum);
+        return sum;
     }
 
     //Напишите программу, которая возвращает true, если двумерный массив является квадратным.
@@ -224,6 +249,105 @@ public class HW6 {
         return min;
     }
 
+
+    //Напишите программу, которая ищет заданный элемент в двумерном массиве и выводит его координаты (строка и столбец).
+
+    public int[] searchIndexXY(int[][] arr, int searcherNum) {
+        int[] savingIndex = new int[2];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == searcherNum) {
+                    savingIndex[0] = i;
+                    savingIndex[1] = j;
+                    break;
+                }
+            }
+        }
+        return savingIndex;
+    }
+
+    //Напишите программу, которая находит среднее значение в каждой строке row двумерного массива и выводит его.
+
+    public double[] findAvgRow(int[][] arr) {
+        int n = arr.length;
+        double[] avgRow = new double[n];
+        for (int i = 0; i < n; i++) {
+            avgRow[i] = findAverageArray(arr[i]);
+        }
+        return avgRow;
+    }
+
+    //Напишите программу, которая транспонирует заданную матрицу (меняет строки на столбцы и наоборот).
+
+    // I have already done in the method changeRowToColumn(arr);
+
+    //Напишите программу, которая находит максимальный и минимальный элементы в двумерном массиве.
+    public int findMin2DArray(int arr[][]) {
+        int min = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min > findMinArr(arr[i])) {
+                min = findMinArr(arr[i]);
+            }
+        }
+        return min;
+    }
+
+    public int findMax2DArray(int arr[][]) {
+        int max = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max < findMaxArr(arr[i])) {
+                max = findMinArr(arr[i]);
+            }
+        }
+        return max;
+    }
+
+    //Напишите программу, которая сортирует строки в двумерном массиве в порядке возрастания.
+
+    public int[][] orderRowASC2D(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            ascOrder(arr[i]);
+        }
+        return arr;
+    }
+
+    //Напишите программу, которая проверяет, является ли заданная матрица симметричной (относительно главной диагонали).
+    // if (i == j)
+    // 00 01 02
+    // 10 11 12
+    // 20 21 22
+
+    public boolean isSymmetric2DArray(int[][] arr) {
+        boolean isSymmetric = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i != j) {
+                    if (arr[i][j] == arr[j][i]) {
+                        isSymmetric = true;
+                    } else
+                        return false;
+                }
+            }
+        }
+        return isSymmetric;
+    }
+
+
+    //Напишите программу, которая поворачивает матрицу на 90 градусов по часовой стрелке.*/
+
+
+    public int[][] changeNinetyPer(int[][] arr) {
+        int row = arr.length;
+        int column = arr[0].length;
+        int[][] change90 = new int[column][row];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                change90[j][row - i - 1] = arr[i][j];
+            }
+        }
+        return change90;
+    }
 
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,10 +19,34 @@ public class Main {
         six.printSecondMinValue(arr);
         six.descOrder(arr);
 
-        int arr2[][] = {{1, 2, 3}, {4, 5, 6}};
-        six.changeRowToColumn(arr2);
+        int arr2[][] = {{1, 3, 3}, {2, 5, 6}, {3, 6, 2} };
+
+        // if (i == j)
+        // 00 01 02
+        // 10 11 12
+        // 20 21 22
+
+        System.out.println("Is sym " + six.isSymmetric2DArray(arr2));
+
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 3; j++){
+                System.out.print(arr2[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("====");
+
+        six.print2DArray(six.changeNinetyPer(arr2));
+
+        System.out.println("===");
         six.sumTwoDArr(arr2);
         six.isArraySquare(arr2);
+
+        double arr4[] = six.findAvgRow(arr2);
+
+
+        System.out.println("X: " + arr4[0] + " Y: " + arr4[1]);
         int[][] ar = six.calculateMultipleTable(5);
         six.print2DArray(ar);
 
@@ -30,6 +54,10 @@ public class Main {
         System.out.println(example.isOdd(4));
 
         example.findNameWeek(9);
+
+
+
+
 
 
 
